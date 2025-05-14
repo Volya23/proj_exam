80ff5ff042ba86baa0d46dab2c4c0e396d1b20c3
Volya23 <v.o.kryzhanivska@gmail.com>
2025-05-14 14:00:49 +0300

    a little change

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         cardNumber: '4111111111111111',
         name: 'yriy',
-        expiry: '09/23',
+        expiry: '09/28',
         cvc: '505',
         balance: 5000,
       },
